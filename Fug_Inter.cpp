f89f580fdaaebed4a011db7e71bd7c6f8697af16
WIN-QHBBKI026OG\User <andriy.kondratuik@gmail.com>
2021-03-05 21:55:57 +0200

    Created PlayerManager class

--- Fug_Inter.cpp
+++ Fug_Inter.cpp
@@ -30,3 +30,65 @@ public:
 	{
 	}
 };
+
+class PlayerManager
+{
+public:
+	vector<Player> players;
+
+	void CreatePlayer(int _id, string _name, int _rank = 0)
+	{
+		Player new_player(_id, _name, _rank);
+		players.push_back(new_player);
+	}
+	Player GetPlayerByName(string _name)
+	{
+		for (auto& elem : players)
+		{
+			if (_name == elem.name)
+				return elem;
+		}
+		Player empty(0, "Player invalid");
+		return empty;
+	}
+	Player GetPlayerById(int _id)
+	{
+		for (auto& elem : players)
+		{
+			if (_id == elem.id)
+				return elem;
+		}
+		Player empty(0, "Player invalid");
+		return empty;
+	}
+	void DeletePlayer(int _id, string _name = "")
+	{
+		this->players;
+		for (int i = 0; i < players.size(); i++)
+		{
+			if (_id == players[i].id || _name == players[i].name)
+			{
+				players.erase(players.begin() + i);
+				break;
+			}
+		}
+	}
+	void ChangeRank(int _id, int rank_change = 0)
+	{
+		this->players;
+		players[_id - 1].rank += rank_change;
+	}
+	void ShowPlayerInfo(string _name, int _id = 0)
+	{
+		for (int i = 0; i < players.size(); i++)
+		{
+			if (_name == players[i].name || _id == players[i].id)
+			{
+				cout << "\nName: " << players[i].name <<
+						"\tId: "   << players[i].id <<
+						"\tRank: " << players[i].rank << "\n";
+				break;
+			}
+		}
+	}
+};
